b915dc668a90b9372988b4892437717c4e6d95a1
TalmonG <118438762+TalmonG@users.noreply.github.com>
2023-02-23 00:54:19 +0000

    interact start

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Interact : MonoBehaviour
 {
+    public GameObject UiObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        UiObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter(Collider player)
     {
-        
+        if (player.gameObject.tag == "Player")
+        {
+            UiObject.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            UiObject.SetActive(false);
+        }
     }
 }
