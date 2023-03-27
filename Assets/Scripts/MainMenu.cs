1057fcdf333d4ffa5561c66ed3be1bbd48be87e2
MaxElam2021 <94044736+MaxElam2021@users.noreply.github.com>
2023-03-27 11:40:41 +0100

    Added Main MenuScript

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public AudioSource sfxPlayer;
+    [SerializeField] GameObject difficultyPanel;
+
+	void Start()
+	{
+		sfxPlayer = GetComponent<AudioSource>();
+        difficultyPanel.SetActive(false);
+	}
+
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene("Game"); //This should be deleted if difficulty options is succesfully added
+        //difficultyPanel.SetActive(true);
+        //Time.timeScale = 0f;
+    }
+
+    /*public void Easy()
+    {
+        SceneManager.LoadScene("EasyGame");
+    }
+
+    public void Normal()
+    {
+        SceneManager.LoadScene("NormalGame");
+
+    }
+
+    public void Hard()
+    {
+        SceneManager.LoadScene("EasyGame");
+    }
+
+    public void BackToMainMenu()
+    {
+        Time.timeScale = 1f;
+        difficultyPanel.SetActive(false);
+    }
+    */
+    
+	public void Credits ()
+    {
+       SceneManager.LoadScene("Credits");
+    }
+
+	public void HowToPlay ()
+    {
+       SceneManager.LoadScene("HowToPlay");
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
