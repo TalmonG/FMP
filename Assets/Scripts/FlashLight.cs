d6e39d7c34ae6d0ad0f108d7e5b8936602bce15e
TalmonG <118438762+TalmonG@users.noreply.github.com>
2023-04-18 10:41:31 +0100

    swave

--- Assets/Scripts/FlashLight.cs
+++ Assets/Scripts/FlashLight.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class FlashLight : MonoBehaviour
 {
+
+    GameObject flashLight;
+    //bool click
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        flashLight.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetMouseButtonDown(0))
+        {
+            flashLight.SetActive(true);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            flashLight.SetActive(true);
+        }
+
     }
 }
